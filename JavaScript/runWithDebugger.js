8faed56c4f43a9f77719303c66f3d5c5c88f6746
Ewe Lin Loo <ewelinloo@gmail.com>
2017-09-23 09:23:36 +1000

    Create runWithDebugger.js
    Exercise from Watch and Code

---
+++ JavaScript/runWithDebugger.js
@@ -0,0 +1,36 @@
+function runWithDebugger() {
+  debugger;
+ 
+  var callback = arguments[0];
+  var callbackArgs = [];
+ 
+  for (i = 1; i < arguments.length; i++){
+    callbackArgs.push(arguments[i]);
+  } 
+
+  callback.apply(this, callbackArgs);
+}
+
+
+
+/*----------------*/
+// Callback with no arguments
+function sayHi(){
+  console.log('Hi');
+}
+
+runWithDebugger(sayHi);
+
+// Callback with one argument
+function sayHiTo(name){
+  console.log('Hi, ' + name);
+}
+
+runWithDebugger(sayHiTo, 'Jane');
+
+//Callback with two arguments
+function sayFullName(firstName, lastName){
+  console.log(firstName + ' ' + lastName);
+}
+
+runWithDebugger(sayFullName, 'Jane', 'Eyre');
