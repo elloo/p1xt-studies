15b4eb06cff2bfe9ef38421d7007467a1e1bbbc6
elloo <ewelinloo@gmail.com>
2018-03-09 02:16:57 +1100

    09/03/2018 update

--- programming-for-the-web-with-javascript/week2/homework3/calc.js
+++ programming-for-the-web-with-javascript/week2/homework3/calc.js
@@ -1,50 +1,71 @@
+// !!!!!!!!!!!!!!!!!
+// Debugging Case #3
+// !!!!!!!!!!!!!!!!!
+
 var val0 = "";   // Empty string clears display after operation button is used
-var val1 = "";   // Val1 for secondary number in operations
-var arithOp;
-var result;
+var val1 = "";   // val1 for secondary number in operations
+var arithOp = "";
+var prevBtn = "";
+var result = "";
 
-$(".num").click(function(){
-
-    if (arithOp){    // Using new container for second value in operation
-        val1 += $(this).html();        
-        $("input").val(val1);  
-        
-    } else {
-        if (arithOp == false && result){ // Begin a new operation when number is pressed after #equalsButton
-            clearData();
-        }
-        
-        // Initial value of new operation
-        val0 += $(this).html();        
-        $("input").val(val0);  
-    }
+$(".num").click(function(){     // For 0-9
     
-});
-
-$(".ops").click(function(){
- 
-    if (result){    // For operations with more than two numbers
-        val0 = parseFloat(result);
-    }    
-    
-    if (arithOp){   // For operations with more than two numbers
-        calculate(val0, val1);
+    if (prevBtn == "="){
         val1 = "";
     }
     
-    arithOp = $(this).html();
+    if (prevBtn == "=" && result){
+        val0 = ""
+        arithOp = "";
+    }
+    
+    if (arithOp || prevBtn == "=" && val1 == "" && val0 != ""){
+        val1 += $(this).html();        
+        $("input").val(val1);         
+    } else {
+        if (arithOp == "" && result ){ // Begin a new operation when number is pressed after #equalsButton
+            clearData();
+        }        
+        val0 += $(this).html();        // Initial value of new operation
+        $("input").val(val0);  
+    }
+    
+    prevBtn = $(this).html();  
+    
+});
+
+$(".ops").click(function(){     // For +, -, /, * buttons
+    
+    if (prevBtn == "="){    // Reset to allow result of previous operation to be used
+        val1 = "";
+        arithOp = "";
+    }
+    
+   // When clicking .ops btn for the second time, consecutively
+    if (arithOp && prevBtn == "+" || prevBtn == "-" || prevBtn == "*" || prevBtn.charCodeAt(0) == "247"){
+        arithOp = $(this).html();    
+    } else          // When clicking .ops btn for the second time, non-consecutively
+    if (arithOp){
+        calculate(val0, val1);
+        val1 = "";
+        arithOp = $(this).html();
+    } else {        // When clicking .ops btn for the first time
+        arithOp = $(this).html();
+    }
+    
+    prevBtn = arithOp;
     
 });
 
 $("#equalsButton").click(function(){
     
-    if (val1 == false){         // If no second value is input
+    if (val1 == ""){         // If no second value is input
         $("input").val(val0); 
     } else {
         calculate(val0, val1);
-        val0 = "";
-        val1 = "";
-    } 
+    }
+    
+    prevBtn = $(this).html();
     
 });
 
@@ -76,11 +97,7 @@ function calculate(x, y){
         result = x * y;
     } else
     if (arithOp.charCodeAt(0) == "247"){ // Using Unicode instead of ASCII as in .html doc
-        if (y == "0"){
-            result = "Infinity";
-        } else {
-            result = x / y;
-        }
+        result = x / y;
     }
         
     $("input").val(result);  
@@ -93,6 +110,7 @@ function clearData(){
     val0 = "";
     val1 = "";
     arithOp = "";
+    prevBtn = "";
     result = "";
     $("input").val("");  
     
