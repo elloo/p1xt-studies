bef7f2510e3d6f7866dc1d35f254681d278f3e77
elloo <ewelinloo@gmail.com>
2018-03-10 02:08:39 +1100

    09/03/2018 update

--- programming-for-the-web-with-javascript/week3/homework4/FontChooser.js
+++ programming-for-the-web-with-javascript/week3/homework4/FontChooser.js
@@ -6,22 +6,18 @@ class FontChooser extends React.Component {
     }
     
     toggleHidden() {
-        if (this.state.hidden){
-            this.setState({ hidden: 'false' });
-        } else {
-            this.setState({ hidden: 'true' });            
-        }
+        this.setState({ hidden: !this.state.hidden });
     }
 
     render() {
         var toggled = this.state.hidden;
 	return(
 	       <div>
-               <input type="checkbox" id="boldCheckbox" hidden=toggled/>
-               <button id="decreaseButton" hidden=toggled>-</button>
-               <span id="fontSizeSpan" hidden=toggled>{this.props.size}</span>
-               <button id="increaseButton" hidden=toggled>+</button>
-               <span id="textSpan" onClick={this.toggleHidden}>{this.props.text}</span>
+               <input type="checkbox" id="boldCheckbox" hidden={toggled}/>
+               <button id="decreaseButton" hidden={toggled}>-</button>
+               <span id="fontSizeSpan" hidden={toggled}>{this.props.size}</span>
+               <button id="increaseButton" hidden={toggled}>+</button>
+               <span id="textSpan" onClick={this.toggleHidden.bind(this)}>{this.props.text}</span>
 	       </div>
 	);
     }
